d0d7c5116936f937429fe76d5bb77f1a8d9e33ba
juicyjung <travelandi01@gmail.com>
2022-05-10 12:52:54 +0900

    add new project

---
+++ WiFi_based_Indoor_Positioning_App/app/src/main/java/sensorteam10/gachon/wi_fi_based_indoor_positioning_app/MainActivity.java
@@ -0,0 +1,14 @@
+package sensorteam10.gachon.wi_fi_based_indoor_positioning_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
