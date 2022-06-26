735431f1ffd98cfe82cd4050466ad4bdc5bffd25
juicyjung <travelandi01@gmail.com>
2022-06-27 00:42:30 +0900

    Euclid algorithm

--- WiFi_based_Indoor_Positioning_App/app/src/main/java/sensorteam10/gachon/wi_fi_based_indoor_positioning_app/PositionData.java
+++ WiFi_based_Indoor_Positioning_App/app/src/main/java/sensorteam10/gachon/wi_fi_based_indoor_positioning_app/PositionData.java
@@ -1,9 +1,15 @@
 package sensorteam10.gachon.wi_fi_based_indoor_positioning_app;
 
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class PositionData implements Serializable {
@@ -41,24 +47,65 @@ public class PositionData implements Serializable {
 		return values;
 	}
 
-	public int uDistance(PositionData arg,ArrayList<Router> friendlyWifis){
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	public int uDistance(PositionData arg, ArrayList<Router> friendlyWifis){
 		int sum=0;
 		int count=0;
-		 for(Map.Entry<String, Integer> e: this.values.entrySet()){
-			 int v;
+
+		List<Map.Entry<String, Integer>> entryList = new LinkedList<>(values.entrySet());
+		entryList.sort((o1, o2) -> values.get(o2.getKey()) - values.get(o1.getKey()));
+
+		List<Map.Entry<String, Integer>> entryList2 = new LinkedList<>(arg.values.entrySet());
+		entryList2.sort((o1, o2) -> arg.values.get(o2.getKey()) - arg.values.get(o1.getKey()));
+
+
+		Log.v("LARGEST 5", String.valueOf(entryList2));
+
+		for(Map.Entry<String, Integer> e: entryList2){
+			if(count == 5)
+				break;
+
+			int v;
+			int v2;
 			//Log.v("Key : ",arg.values.get(e.getKey()).toString());
-			 if(isFriendlyWifi(friendlyWifis,e.getKey()) && arg.values.containsKey(e.getKey()))
-				 {
-				  v=arg.values.get(e.getKey());
-				  sum+=Math.pow((v-e.getValue()),2);
-				  count++;
-				 }			 			 		 
+			if(values.containsKey(e.getKey()))
+			{
+				v= values.get(e.getKey());
+				v2 = e.getValue();
+				sum+=Math.pow((v - v2),2);
+				count++;
+
+				Log.v("LARGEST 5",  e.getKey() + e.getValue());
 			}
-		 if(count<MINIMUM_COMMON_ROUTERS){
-			 sum=MAX_DISTANCE;
-		 }
-		 
-		 return sum;
+			else{
+				sum+=10000;
+				count++;
+			}
+		}
+		if(count<MINIMUM_COMMON_ROUTERS){
+			sum=MAX_DISTANCE;
+		}
+
+//		for(Map.Entry<String, Integer> e: entryList){
+//			if(count == 5)
+//				break;
+//
+//			int v;
+//			//Log.v("Key : ",arg.values.get(e.getKey()).toString());
+//			if(isFriendlyWifi(friendlyWifis,e.getKey()) && arg.values.containsKey(e.getKey()))
+//			{
+//				v=arg.values.get(e.getKey());
+//				sum+=Math.pow((v-e.getValue()),2);
+//				count++;
+//
+//				Log.v("LARGEST 5",  e.getKey() + e.getValue());
+//			}
+//		}
+//		if(count<MINIMUM_COMMON_ROUTERS){
+//			sum=MAX_DISTANCE;
+//		}
+
+		return sum;
 	}
 
     private boolean isFriendlyWifi(ArrayList<Router> wifis,String bssid){
